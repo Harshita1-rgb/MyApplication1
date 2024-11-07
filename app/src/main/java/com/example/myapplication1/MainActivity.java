842eb92f223d253f61643cf0df4a54c4dfa86ee2
HARSHITA <nistalamani@gmail.com>
2024-11-08 01:57:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication1;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+        }
+    }
